16f1094e5014763ec1275b86e6be639d4ed20dca
stephaff <affalistephane@gmail.com>
2022-07-21 12:48:55 +0000

    design menu deroulant

--- js/details--articles.js
+++ js/details--articles.js
@@ -34,3 +34,16 @@ logo.addEventListener('click', actionLogo);
 function actionLogo(){
     location = 'index.html';
 }
+
+let headerRight = document.querySelector('.header-right');
+
+headerRight.addEventListener('mouseenter', displayClientNavbarOn);
+headerRight.addEventListener('mouseleave', displayClientNavbarOff);
+
+function displayClientNavbarOn(){
+    document.querySelector('.client-navbar').classList.add('display-client-navbar');
+}
+
+function displayClientNavbarOff(){
+    document.querySelector('.client-navbar').classList.remove('display-client-navbar');
+}
